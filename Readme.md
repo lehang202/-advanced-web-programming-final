46b1510a9be063fc03c3376274f207d95044e1a9
lehang202 <71984277+lehang202@users.noreply.github.com>
2022-05-16 14:33:11 +0700

    Sườn web

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Lập trình web nâng cao
+
+đã cài 
+# npm init
+# npm install express
+
+----
+# cd Source
+# npm install ejs
---
+++ Source/index.js
@@ -0,0 +1,17 @@
+//cài đặt module như express, body-parser(để xử lý form) và nodemon (để restart lại sever mỗi khi save code)
+//sử dụng lệnh 'npm start' có tác dụng chạy chương trình bằng nodemon, và từ sau không cần phải khởi động lại server thu công mỗi khi lưu code 
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set("views", "./views");
+
+app.get('/', (req, res) => {
+
+    res.render('homePage');
+})
+
+app.get('/login', (req, res) => {
+    return res.end('login');
+})
+app.listen(8080, () => console.log('http://localhost:8080'))
